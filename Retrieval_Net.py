65f660d94cac901b303412792268f11b0781e3fd
Zahin Sufiyan <55498795+zahinsufiyan@users.noreply.github.com>
2024-05-14 17:18:56 -0600

    Update Retrieval_Net.py

--- Retrieval_Net.py
+++ Retrieval_Net.py
@@ -1,5 +1,4 @@
 import gym
-gym.make('Reacher-v2')
 import copy
 import numpy as np
 import torch
@@ -9,67 +8,49 @@ import utils
 import csv
 import GPUtil
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-with open('gpu_data_part1.csv', mode='w', newline='') as file:
+with open('gpu_usage_log.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Algorithm', 'Frame_idx', 'GPU Load (%)', 'GPU Memory (MB)'])
 
-
-class RewardShapeWrapper(gym.Wrapper):
-    def _reward(self, r, done):
-        sparse_r = 0
-        if done and r >= -0.09:
-            sparse_r = r + 0.2
-        return sparse_r
+class RewardModifier(gym.Wrapper):
+    def _modify_reward(self, reward, done):
+        adjusted_reward = 0
+        if done and reward >= -0.09:
+            adjusted_reward = reward + 0.2
+        return adjusted_reward
 
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
-        if done == False:
-            reward = 0
-        else:
-            print(info)
-            reward = info['reward_dist']
+        reward = 0 if not done else info['reward_dist']
         return observation, reward, done, info
 
-
-class Retrieval(nn.Module):
+class RetrievalNetwork(nn.Module):
     def __init__(self, state_dim, action_dim):
-        super(Retrieval, self).__init__()
-
-        self.l1 = nn.Linear(state_dim + action_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, 256)
-        self.l4 = nn.Linear(256, state_dim)
+        super(RetrievalNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim + action_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.fc4 = nn.Linear(256, state_dim)
 
     def forward(self, state, action):
-        sa = torch.cat([state, action], -1)
+        combined = torch.cat([state, action], dim=-1)
+        x = F.relu(self.fc1(combined))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
 
-        q1 = F.relu(self.l1(sa))
-        q1 = F.relu(self.l2(q1))
-        q1 = F.relu(self.l3(q1))
-        q1 = self.l4(q1)
-
-        return q1
-
-
-class TrainRetrieval(object):
+class RetrievalTrainer:
     def __init__(
-            self,
-            state_dim,
-            action_dim,
-            max_action,
-            discount=0.99,
-            tau=0.005,
-            policy_noise=0.2,
-            noise_clip=0.5,
-            policy_freq=2
+        self, state_dim, action_dim, max_action, 
+        discount=0.99, tau=0.005, policy_noise=0.2, 
+        noise_clip=0.5, policy_freq=2
     ):
-
-        self.retrieval = Retrieval(state_dim, action_dim).to(device)
-        self.retrieval_target = copy.deepcopy(self.retrieval)
-        self.retrieval_optimizer = torch.optim.Adam(self.retrieval.parameters(), lr=3e-4)
+        self.retrieval_net = RetrievalNetwork(state_dim, action_dim).to(device)
+        self.retrieval_net_target = copy.deepcopy(self.retrieval_net)
+        self.optimizer = torch.optim.Adam(self.retrieval_net.parameters(), lr=3e-4)
 
         self.max_action = max_action
         self.discount = discount
@@ -78,88 +59,80 @@ class TrainRetrieval(object):
         self.noise_clip = noise_clip
         self.policy_freq = policy_freq
 
-        self.total_it = 0
+        self.iterations = 0
 
     def train(self, replay_buffer, batch_size=256):
-        self.total_it += 1
+        self.iterations += 1
 
         state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
 
-        pre_state = self.retrieval(next_state, action)
+        predicted_state = self.retrieval_net(next_state, action)
 
-        # Compute retrieval loss
-        retrieval_loss = F.mse_loss(pre_state, state)
-        print(retrieval_loss)
+        loss = F.mse_loss(predicted_state, state)
+        print(loss)
 
-        # Optimize the retrieval
-        self.retrieval_optimizer.zero_grad()
-        retrieval_loss.backward()
-        self.retrieval_optimizer.step()
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
     def save(self, filename):
-        torch.save(self.retrieval.state_dict(), filename + "_retrieval")
-        torch.save(self.retrieval_optimizer.state_dict(), filename + "_retrieval_optimizer")
+        torch.save(self.retrieval_net.state_dict(), filename + "_retrieval")
+        torch.save(self.optimizer.state_dict(), filename + "_optimizer")
 
-
-max_episode_steps = 50
-env = RewardShapeWrapper(gym.make('Reacher-v2'))
-test_env = RewardShapeWrapper(gym.make('Reacher-v2'))
+max_steps_per_episode = 50
+env = RewardModifier(gym.make('Reacher-v2'))
+test_env = RewardModifier(gym.make('Reacher-v2'))
 
 action_dim = env.action_space.shape[0]
 state_dim = env.observation_space.shape[0]
 max_action = float(env.action_space.high[0])
-hidden_dim = 256
 
-policy = TrainRetrieval(state_dim, action_dim, max_action)
+trainer = RetrievalTrainer(state_dim, action_dim, max_action)
 
-replay_buffer_size = 1000000
 replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 
-max_frames = 1000000
-start_timesteps = 1000
+total_frames = 1000000
+initial_timesteps = 1000
 
-frame_idx = 0
-rewards = []
+current_frame = 0
+all_rewards = []
 test_rewards = []
 batch_size = 256
-test_epoch = 0
-expl_noise = 0.1
+test_interval = 0
+exploration_noise = 0.1
 episode_reward = 0
-episode_timesteps = 0
-episode_num = 0
+episode_steps = 0
+episode_count = 0
 
 state, done = env.reset(), False
 
-while frame_idx < max_frames:
-
-    episode_timesteps += 1
+while current_frame < total_frames:
+    episode_steps += 1
     action = env.action_space.sample()
 
     next_state, reward, done, _ = env.step(action)
-    done_bool = float(done) if episode_timesteps < max_episode_steps else 0
-    replay_buffer.add(state, action, next_state, reward, done_bool)
+    done_flag = float(done) if episode_steps < max_steps_per_episode else 0
+    replay_buffer.add(state, action, next_state, reward, done_flag)
 
     state = next_state
     episode_reward += reward
 
-    if frame_idx >= start_timesteps:
-        policy.train(replay_buffer, batch_size)
+    if current_frame >= initial_timesteps:
+        trainer.train(replay_buffer, batch_size)
 
-    if frame_idx % 10000 == 0 and frame_idx > 0:
+    if current_frame % 10000 == 0 and current_frame > 0:
         gpu = GPUtil.getGPUs()[0]
-        with open('gpu_data_part1.csv', mode='a', newline='') as file:
+        with open('gpu_usage_log.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["CFlowNets-Part1", frame_idx, gpu.load*100, gpu.memoryUsed])
+            writer.writerow(["RetrievalTrainer", current_frame, gpu.load * 100, gpu.memoryUsed])
 
-    if frame_idx >= start_timesteps and frame_idx % 10000 == 0:
-        torch.save(policy.retrieval.state_dict(), 'retrieval_reacher_sparse.pkl')
+    if current_frame >= initial_timesteps and current_frame % 10000 == 0:
+        torch.save(trainer.retrieval_net.state_dict(), 'retrieval_reacher_model.pkl')
 
     if done:
         state, done = env.reset(), False
         episode_reward = 0
-        episode_timesteps = 0
-        episode_num += 1
+        episode_steps = 0
+        episode_count += 1
 
-    frame_idx += 1
-
-
+    current_frame += 1
